20b07aac939a377e7cd9680ad93a0822952db74e
Brad Clawsie <brad@b7j0c.org>
2026-02-13 02:42:16 -0600

    add sql and related model pkg

---
+++ pkg/model/model.go
@@ -0,0 +1,11 @@
+// Package model provides constants for all models.
+package model
+
+const (
+	RoleNormal        = 1
+	RoleAdmin         = 2
+	RoleTest          = 3
+	StatusUnconfirmed = 1
+	StatusActive      = 2
+	StatusInactive    = 3
+)
